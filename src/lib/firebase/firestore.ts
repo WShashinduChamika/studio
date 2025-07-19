4f767a5820434a60c0b6dd0edd96aa6e23118f7c
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2025-07-19 23:49:34 +0530

    Second Commit

--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -42,6 +42,7 @@ export const addTimeLog = (log: TimeLogInput) => {
 
 // Get real-time updates for a user's time logs
 // Set first commit
+// Set second commit
 export const getTimeLogsStream = (
   userId: string,
   callback: (logs: TimeLog[]) => void

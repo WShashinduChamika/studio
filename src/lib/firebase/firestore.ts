57d0899a132f7424ecd5b9eff4a7520575e82e67
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2025-07-19 23:50:46 +0530

    Third Commit

--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -44,7 +44,6 @@ export const addTimeLog = (log: TimeLogInput) => {
 // Set first commit
 // Set second commit
 // Set third commit
-// Set fourth commit 
 export const getTimeLogsStream = (
   userId: string,
   callback: (logs: TimeLog[]) => void
